24d11544404c85fa668d612a69fa515cc0422be6
José Carlos <jose.carlos@fdez.se>
2025-03-11 19:22:21 +0100

    Fix: Update database username environment variable in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +(process.env.DB_PORT ?? 5432),
-      username: process.env.DB_USERNAME,
+      username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       synchronize: true,
