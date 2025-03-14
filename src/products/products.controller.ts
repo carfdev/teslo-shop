473102f264d94bf4f19544e03e8dcb2c608013f0
José Carlos <jose.carlos@fdez.se>
2025-03-14 16:17:36 +0100

    Add: Implementar autenticación en el controlador de productos y semilla, incluyendo roles de administrador

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -13,26 +13,32 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { Auth } from '../auth/decorators';
+import { ValidRoles } from '../auth/interfaces';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @Auth(ValidRoles.admin)
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
+  @Auth()
   @Get()
   findAll(@Query() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
+  @Auth()
   @Get(':term')
   findOne(@Param('term') term: string) {
     return this.productsService.findOne(term);
   }
 
+  @Auth(ValidRoles.admin)
   @Patch(':id')
   update(
     @Param('id', ParseUUIDPipe) id: string,
@@ -41,6 +47,7 @@ export class ProductsController {
     return this.productsService.update(id, updateProductDto);
   }
 
+  @Auth(ValidRoles.admin)
   @Delete(':id')
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.remove(id);
