78f34ae1c9fba0fa2705ca8eaab6ad5e06447f92
José Carlos <jose.carlos@fdez.se>
2025-03-14 11:48:03 +0100

    Refactor: Update Auth module to use CreateUserDto and integrate CommonService for user creation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { TypeORMError } from 'src/common/interfaces/typeORM-error.interface';
+import { CommonService } from 'src/common/common.service';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
-
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly commonService: CommonService,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const user = this.userRepository.create(createUserDto);
+      return await this.userRepository.save(user);
+    } catch (e) {
+      const error = e as TypeORMError;
+      this.commonService.handleExeptions(error);
+    }
   }
 }
