42a442b7ca4b897894730e5e5e305ef79375581e
José Carlos <jose.carlos@fdez.se>
2025-03-15 09:56:19 +0100

    Add: Implementar manejo de conexión y desconexión de clientes en el gateway de WebSocket

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,7 +1,22 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class MessageWsGateway {
+export class MessageWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private readonly messageWsService: MessageWsService) {}
+
+  handleConnection(client: Socket) {
+    console.log('Client connected', client.id);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('Client disconnected', client.id);
+  }
 }
